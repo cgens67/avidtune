039fdd542cf137056e0135a62139fc95d9a0c735
cgens67 <cgenshin587@gmail.com>
2026-06-04 03:42:03 +0800

    change version to 1.0.1 and versioncode to 2

--- app/src/main/java/com/cgens67/avidtune/ui/menu/LyricsMenu.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/menu/LyricsMenu.kt
@@ -5,6 +5,7 @@ import android.app.SearchManager
 import android.content.Intent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
