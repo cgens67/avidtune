7af7a91939ad783a22b0708bc41e80f0189d8d56
cgens67 <cgenshin587@gmail.com>
2026-06-20 19:58:10 +0800

    Add stringPreferencesKey import to PreferenceKeys.kt
    Added stringPreferencesKey import for preference management.

--- app/src/main/java/com/cgens67/avidtune/constants/PreferenceKeys.kt
+++ app/src/main/java/com/cgens67/avidtune/constants/PreferenceKeys.kt
@@ -4,6 +4,7 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
