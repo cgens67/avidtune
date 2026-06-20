88751e61830fd2b58e99a57c24032d49fe78cee8
cgens67 <cgenshin587@gmail.com>
2026-06-21 00:03:51 +0800

    Add filter for music videos in NewReleaseViewModel

--- app/src/main/java/com/cgens67/avidtune/viewmodels/NewReleaseViewModel.kt
+++ app/src/main/java/com/cgens67/avidtune/viewmodels/NewReleaseViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.viewModelScope
 import com.cgens67.innertube.YouTube
 import com.cgens67.innertube.models.AlbumItem
 import com.cgens67.innertube.models.filterExplicit
+import com.cgens67.innertube.models.filterMusicVideos
 import com.cgens67.avidtune.constants.HideExplicitKey
+import com.cgens67.avidtune.constants.HideMusicVideosKey
 import com.cgens67.avidtune.constants.LastNewReleaseCheckKey
 import com.cgens67.avidtune.db.MusicDatabase
 import com.cgens67.avidtune.utils.dataStore
@@ -64,6 +66,7 @@ constructor(
                                 } ?: Int.MAX_VALUE
                             firstArtistKey
                         }.filterExplicit(context.dataStore.get(HideExplicitKey, false))
+                         .filterMusicVideos(context.dataStore.get(HideMusicVideosKey, false))
 
                     _newReleaseAlbums.value = sortedAlbums
 
