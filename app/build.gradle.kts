4180fdd357da3782d757f56e06c8103acd649c1f
cgens67 <cgenshin587@gmail.com>
2026-06-22 16:08:51 +0800

    Bump versionCode to 5 and versionName to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.cgens67.avidtune"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
