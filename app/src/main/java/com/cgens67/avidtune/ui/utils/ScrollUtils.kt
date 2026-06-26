575a4baf9f4d97c11f14d70b324320dd52845d30
cgens67 <cgenshin587@gmail.com>
2026-06-26 12:38:35 +0800

    Refactor scrolling detection using LaunchedEffect

--- app/src/main/java/com/cgens67/avidtune/ui/utils/ScrollUtils.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/utils/ScrollUtils.kt
@@ -4,56 +4,65 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 
 @Composable
 fun LazyListState.isScrollingUp(): Boolean {
-    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
-    return remember(this) {
-        derivedStateOf {
-            if (previousIndex != firstVisibleItemIndex) {
-                previousIndex > firstVisibleItemIndex
-            } else {
-                previousScrollOffset >= firstVisibleItemScrollOffset
-            }.also {
-                previousIndex = firstVisibleItemIndex
-                previousScrollOffset = firstVisibleItemScrollOffset
+    var isScrollingUp by remember { mutableStateOf(true) }
+    LaunchedEffect(this) {
+        var previousIndex = firstVisibleItemIndex
+        var previousScrollOffset = firstVisibleItemScrollOffset
+        snapshotFlow { firstVisibleItemIndex to firstVisibleItemScrollOffset }
+            .collect { (index, offset) ->
+                if (previousIndex != index) {
+                    isScrollingUp = previousIndex > index
+                } else if (previousScrollOffset != offset) {
+                    isScrollingUp = previousScrollOffset > offset
+                }
+                previousIndex = index
+                previousScrollOffset = offset
             }
-        }
-    }.value
+    }
+    return isScrollingUp
 }
 
 @Composable
 fun LazyGridState.isScrollingUp(): Boolean {
-    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
-    return remember(this) {
-        derivedStateOf {
-            if (previousIndex != firstVisibleItemIndex) {
-                previousIndex > firstVisibleItemIndex
-            } else {
-                previousScrollOffset >= firstVisibleItemScrollOffset
-            }.also {
-                previousIndex = firstVisibleItemIndex
-                previousScrollOffset = firstVisibleItemScrollOffset
+    var isScrollingUp by remember { mutableStateOf(true) }
+    LaunchedEffect(this) {
+        var previousIndex = firstVisibleItemIndex
+        var previousScrollOffset = firstVisibleItemScrollOffset
+        snapshotFlow { firstVisibleItemIndex to firstVisibleItemScrollOffset }
+            .collect { (index, offset) ->
+                if (previousIndex != index) {
+                    isScrollingUp = previousIndex > index
+                } else if (previousScrollOffset != offset) {
+                    isScrollingUp = previousScrollOffset > offset
+                }
+                previousIndex = index
+                previousScrollOffset = offset
             }
-        }
-    }.value
+    }
+    return isScrollingUp
 }
 
 @Composable
 fun ScrollState.isScrollingUp(): Boolean {
-    var previousScrollOffset by remember(this) { mutableStateOf(value) }
-    return remember(this) {
-        derivedStateOf {
-            (previousScrollOffset >= value).also {
-                previousScrollOffset = value
+    var isScrollingUp by remember { mutableStateOf(true) }
+    LaunchedEffect(this) {
+        var previousScrollOffset = value
+        snapshotFlow { value }
+            .collect { offset ->
+                if (previousScrollOffset != offset) {
+                    isScrollingUp = previousScrollOffset > offset
+                }
+                previousScrollOffset = offset
             }
-        }
-    }.value
+    }
+    return isScrollingUp
 }
