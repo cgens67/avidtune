41d246b9a8c89658e5633f890e07c2847a75f227
cgens67 <cgenshin587@gmail.com>
2026-07-04 16:58:32 +0800

    Add PlayerClientOrderKey to PreferenceKeys

--- app/src/main/java/com/cgens67/avidtune/constants/PreferenceKeys.kt
+++ app/src/main/java/com/cgens67/avidtune/constants/PreferenceKeys.kt
@@ -137,6 +137,7 @@ val PlaylistEditLockKey = booleanPreferencesKey("playlistEditLock")
 val QuickPicksKey = stringPreferencesKey("discover")
 val PreferredLyricsProviderKey = stringPreferencesKey("lyricsProvider")
 val LyricsProviderOrderKey = stringPreferencesKey("lyricsProviderOrder")
+val PlayerClientOrderKey = stringPreferencesKey("playerClientOrder")
 val QueueEditLockKey = booleanPreferencesKey("queueEditLock")
 
 val LyricFontSizeKey = intPreferencesKey("lyricFontSize")
