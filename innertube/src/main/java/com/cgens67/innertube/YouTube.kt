65d509c8999fe64b16cc84c98854baff806eee0e
cgens67 <cgenshin587@gmail.com>
2026-08-15 13:00:30 +0800

    Modify player function to include poToken parameter
    Added optional poToken parameter to player function.

--- innertube/src/main/java/com/cgens67/innertube/YouTube.kt
+++ innertube/src/main/java/com/cgens67/innertube/YouTube.kt
@@ -62,6 +62,7 @@ import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 import java.net.Proxy
+import java.util.*
 import kotlin.random.Random
 
 /**
@@ -781,8 +782,8 @@ object YouTube {
         innerTube.deletePlaylist(WEB_REMIX, playlistId)
     }
 
-    suspend fun player(videoId: String, playlistId: String? = null, client: YouTubeClient, signatureTimestamp: Int? = null): Result<PlayerResponse> = runCatching {
-        innerTube.player(client, videoId, playlistId, signatureTimestamp).body<PlayerResponse>()
+    suspend fun player(videoId: String, playlistId: String? = null, client: YouTubeClient, signatureTimestamp: Int? = null, poToken: String? = null): Result<PlayerResponse> = runCatching {
+        innerTube.player(client, videoId, playlistId, signatureTimestamp, poToken).body<PlayerResponse>()
     }
 
     suspend fun registerPlayback(playlistId: String? = null, playbackTracking: String) = runCatching {
