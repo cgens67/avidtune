f692827c5e0f935d5004ab1482e5b1be33b7a8ce
cgens67 <cgenshin587@gmail.com>
2026-08-23 03:05:03 +0800

    Update SearchSuggestionPage.kt

--- innertube/src/main/java/com/cgens67/innertube/pages/SearchSuggestionPage.kt
+++ innertube/src/main/java/com/cgens67/innertube/pages/SearchSuggestionPage.kt
@@ -10,6 +10,7 @@ import com.cgens67.innertube.models.PlaylistItem
 import com.cgens67.innertube.models.PodcastItem
 import com.cgens67.innertube.models.SongItem
 import com.cgens67.innertube.models.YTItem
+import com.cgens67.innertube.models.oddElements
 import com.cgens67.innertube.models.splitBySeparator
 import com.cgens67.innertube.utils.parseTime
 
