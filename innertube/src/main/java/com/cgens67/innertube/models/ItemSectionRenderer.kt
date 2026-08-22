40eb31885844c0a846baa956bc147ad466eb0634
cgens67 <cgenshin587@gmail.com>
2026-08-23 02:22:29 +0800

    Update ItemSectionRenderer.kt

--- innertube/src/main/java/com/cgens67/innertube/models/ItemSectionRenderer.kt
+++ innertube/src/main/java/com/cgens67/innertube/models/ItemSectionRenderer.kt
@@ -4,11 +4,24 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ItemSectionRenderer(
-    val contents: List<Content>? = null,
+    val contents: List<Content>?,
+    val header: Header? = null,
 ) {
     @Serializable
     data class Content(
         val musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer? = null,
         val musicShelfRenderer: MusicShelfRenderer? = null,
+        val musicPlaylistShelfRenderer: MusicPlaylistShelfRenderer? = null,
+        val gridRenderer: GridRenderer? = null,
+    )
+
+    @Serializable
+    data class Header(
+        val itemSectionTabbedHeaderRenderer: ItemSectionTabbedHeaderRenderer?,
+    )
+
+    @Serializable
+    data class ItemSectionTabbedHeaderRenderer(
+        val title: Runs?,
     )
 }
