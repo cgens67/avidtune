369a19c2b969643f71821eb950b94169562f8603
cgens67 <cgenshin587@gmail.com>
2026-08-23 00:57:26 +0800

    Create MusicMultiRowImageItemRenderer.kt

---
+++ innertube/src/main/java/com/cgens67/innertube/models/MusicMultiRowImageItemRenderer.kt
@@ -0,0 +1,11 @@
+package com.cgens67.innertube.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MusicMultiRowImageItemRenderer(
+    val title: Runs,
+    val subtitle: Runs,
+    val thumbnail: ThumbnailRenderer,
+    val onTap: NavigationEndpoint,
+)
