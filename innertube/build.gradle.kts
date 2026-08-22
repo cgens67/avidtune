ee30f06a6c2c56e93bf4c0e55c5d947debbe3310
cgens67 <cgenshin587@gmail.com>
2026-08-23 02:36:20 +0800

    Update build.gradle.kts

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -16,6 +16,5 @@ dependencies {
     implementation(libs.ktor.client.encoding)
     implementation(libs.brotli)
     implementation(libs.newpipe.extractor)
-    implementation(libs.timber)
     testImplementation(libs.junit)
 }
