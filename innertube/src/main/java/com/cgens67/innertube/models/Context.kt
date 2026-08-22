c3670983258f1e708f6a97609ced206f003c27fd
cgens67 <cgenshin587@gmail.com>
2026-08-23 03:03:45 +0800

    Update Context.kt

--- innertube/src/main/java/com/cgens67/innertube/models/Context.kt
+++ innertube/src/main/java/com/cgens67/innertube/models/Context.kt
@@ -13,7 +13,12 @@ data class Context(
     data class Client(
         val clientName: String,
         val clientVersion: String,
-        val osVersion: String?,
+        val userAgent: String? = null,
+        val osName: String? = null,
+        val osVersion: String? = null,
+        val deviceMake: String? = null,
+        val deviceModel: String? = null,
+        val androidSdkVersion: String? = null,
         val gl: String,
         val hl: String,
         val visitorData: String?,
